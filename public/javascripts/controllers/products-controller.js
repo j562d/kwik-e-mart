027784b942624219bf25c87d8fca684e4f41547c
Jimmy Dong <jndong88@gmail.com>
2017-02-27 17:11:13 -0800

    delete products

--- public/javascripts/controllers/products-controller.js
+++ public/javascripts/controllers/products-controller.js
@@ -4,9 +4,9 @@
 angular.module('app')
 .controller('ProductsController', ProductsController);
 
-ProductsController.$inject = ['productService', '$stateParams'];
+ProductsController.$inject = ['productService', '$stateParams', '$state'];
 
-function ProductsController(productService, $stateParams) {
+function ProductsController(productService, $stateParams, $state) {
   var vm = this;
 
   vm.products = productService.query();
@@ -17,13 +17,15 @@ function ProductsController(productService, $stateParams) {
   })
 
 
-
-  // vm.delProduct = function(product) {
-  //   productService.$delete(function() {
-  //     vm.products.splice(vm.products.findIndex(t => t._id === productService._id), 1);
-  //     // above is more performant than vm.products = Todo.query();
-  //   });
-  // };
+  vm.delProduct = function(product) {
+    vm.product.$delete(function() {
+      console.log(vm.product);
+      // vm.products.splice(vm.products.findIndex(t => t._id === product._id), 1);
+      vm.products = productService.query();
+      $state.go('home');
+      // above is more performant than vm.products = Todo.query();
+    });
+  };
 
 }
 
